f042582bd2d3c949bc87771a81f3cb577b36f63b
Peter <schmidt.multimedia@gmail.com>
2022-01-24 10:29:49 +0100

    getElement() / 2 Varianten

--- arr_01.js
+++ arr_01.js
@@ -13,6 +13,44 @@ const controls = ["<", "</", ">"];
 const tags = ["html","head","head","body","h1","h1","p","p","body","html"];
 let stack = [];
 
+// Modul: HTML-Synthese | Test
+// output(getHTML());
+function getHTML() {
+
+    let htmlStr = "";
+
+    for (let i = 0; i < tags.length; i++) {
+        htmlStr += getElement(tags[i],"open");
+        // htmlStr += getElement(tags[i],"close");
+    }
+
+    return htmlStr;
+}
+
+// Modul: Zusammenbau der Elements: <tagStr> --> Tests:
+output(getElement(tags[1],"open"));
+output(getElement(tags[1],"close"));
+output(getElement(tags[1]));
+function getElement(tag,op) {
+    // switch (op) {
+    //     case "open": 
+    //         return controls[0] + tag + controls[2];
+    //     case "close":
+    //         return controls[1] + tag + controls[2];
+    //     default:
+    //       return "#!?";
+    // }
+
+    if (op == "open") {
+        return controls[0] + tag + controls[2];
+    } 
+    else if(op == "close"){
+        return controls[1] + tag + controls[2];
+    }
+    else {
+        return "#!?";
+    }
+}
 
 
 // Modul: Ausgabe | Test
